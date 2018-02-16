228ed58a57c0462858a05d8cb2759560b792f0c2
Milica Mladjenovic <milicamladjenovic95@gmail.com>
2018-02-16 17:50:53 +0100

    pre merge

---
+++ DS2_keras.py
@@ -0,0 +1,190 @@
+import csv
+import os
+
+import math
+from keras.models import Sequential, model_from_json
+from keras.layers import Dense, np, Dropout
+from keras.optimizers import SGD, RMSprop
+from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn import linear_model
+from random import shuffle
+from util import feature_selection, normalize_data, divide_data_set, find_minmax, denormalize_data
+
+
+def read(file_name):
+    print("\t\tReading data from file...")
+    X = []
+    Y = []
+    with open(file_name, 'rt') as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        rows = []
+
+        for row in reader:
+            rows.append(row)
+
+        shuffle(rows)
+        for row in rows:
+            Y.append([float(_x) for _x in row[4:6]])
+            del row[4:6]
+            del row[0]
+            del row[2]
+            X.append([int(_y) if _y.isdigit() else float(_y) for _y in row])
+
+    return X, Y
+
+
+def create_model():
+    model = Sequential()
+
+    model.add(Dense(units=8, activation='relu', input_dim=10))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=8, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1, activation='relu'))
+
+    sgd = SGD(lr=0.1, momentum=0.9, decay=0.0, nesterov=False)
+    model.compile(loss='mean_squared_error',
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+    return model
+
+
+def train():
+    x_data, y_data = read(os.getcwd() + "\datasets\\2\parkinsons_updrs.data.csv")
+
+    reduces = [y[0] for y in y_data]
+    x_data = feature_selection(np.array(x_data), np.array(reduces)).tolist()
+
+    x_data, m, s = normalize_data(x_data)
+
+    x_train, x_validation, x_test = divide_data_set(x_data)
+    y_train, y_validation, y_test = divide_data_set(y_data)
+
+    motor_x_train = np.array([[y[0]] for y in y_train])
+    total_x_train = np.array([[y[1]] for y in y_train])
+    motor_x_test = np.array([[y[0]] for y in y_test])
+    total_x_test = np.array([[y[1]] for y in y_test])
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+    print("\t\tFinished divide data...")
+    n_x_train = np.array(x_train)
+    n_y_train = np.array([[y[0]] for y in y_train])
+    n_y_train = np.array(y_train)
+    print("\t\tTraining neural network...")
+    # Step 6: train network
+    for i in range(0, 10):
+        net = create_neural_network(n_x_train, n_y_train, path=os.getcwd() + "\\keras853_50ep", lr=(i+1)/10)
+        test(x_test, y_test, net, i)
+
+
+def train_reg(X, Y, fn, X_test, Y_test, seed=7):
+    np.random.seed(seed)
+    estimator = KerasRegressor(build_fn=fn, epochs=100, batch_size=128, verbose=0)
+    kfold = KFold(n_splits=10, random_state=seed)
+    # results = cross_val_score(pipeline, X, Y, cv=kfold)
+    results = cross_val_score(estimator, X, Y, cv=kfold)
+    print(results)
+    print('Result: %.2f (%.2f) MSE' % (results.mean(), results.std()))
+
+    estimator.fit(X, Y)
+    netOutput = estimator.predict(X_test)
+
+    print("Loss and metrics")
+    print(rmse(netOutput, Y_test))
+
+
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+
+def test(x_test, y_test, net, i=0):
+    # simulate network, netOutput is y values
+    # loss_and_metrics = net.evaluate(x_test, y_test, batch_size=150)
+    results = net.predict(x_test)
+    print("Results ")
+    print(results)
+    print(y_test)
+
+    print("Loss and metrics for :" + str(i))
+    print(rmse(results, y_test))
+
+
+def save_model(model, path):
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(path + ".json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(path + ".model.h5")
+    print("Saved model to disk")
+
+
+def load_model(path):
+    json_file = open(path + '.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(path + "model.h5")
+    print("Loaded model from disk")
+    loaded_model.compile(loss='mean_squared_error',
+                         optimizer='sgd',
+                         metrics=['accuracy'])
+    return loaded_model
+
+
+def create_neural_network(x_train, y_train, path, lr=0.1, epochs=50, batch=50):
+    # if os.path.isfile(path):
+    #     return load_model(path)
+
+    model = Sequential()
+
+    model.add(Dense(units=8, activation='relu', input_dim=len(x_train[0])))
+    model.add(Dropout(0.1))
+    model.add(Dense(units=8, activation='tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(units=8, activation='tanh'))
+    model.add(Dropout(0.1))
+    model.add(Dense(units=len(y_train[0]), activation='relu'))
+
+    sgd = SGD(lr=lr, momentum=0.1, decay=0.0, nesterov=False)
+    rmsprop = RMSprop(lr=lr)
+    model.compile(loss='mean_squared_error',
+                  optimizer=sgd,
+                  metrics=['mae'])
+
+    model.fit(np.array(x_train), np.array(y_train), epochs=epochs, batch_size=batch, verbose=0)
+    # serialize model to JSON
+    save_model(model, path)
+
+    net = model
+
+    return net
+
+
+def test_sgd_regressor(x_data, y_data, x_test, y_test, alpha=0.1):
+    clf = linear_model.SGDRegressor(shuffle=True, tol=0.2, loss='squared_loss', alpha=alpha)
+    clf.fit(x_data, y_data)
+    result = clf.predict(x_test)
+    score = clf.score(x_test, y_test)
+    print("Loss and metrics")
+    print(rmse(result, y_test))
+    print(score)
+    print(result)
+
+
+def test_lasso_regressor(x_data, y_data, x_test, y_test, alpha=0.1):
+    clf = clf = linear_model.Lasso(alpha=alpha)
+    clf.fit(x_data, y_data)
+    result = clf.predict(x_test)
+    score = clf.score(x_test, y_test)
+    print("Loss and metrics")
+    print(rmse(result, y_test))
+    print(score)
+    print(result)
--- util.py
+++ util.py
@@ -2,12 +2,16 @@ import os
 import neurolab as nl
 import numpy as np
 import math
+import pylab as pl
+from sklearn.preprocessing import MinMaxScaler
+from neurolab.trans import SoftMax, TanSig
 from sklearn.feature_selection import SelectKBest, f_classif, f_regression, mutual_info_classif, mutual_info_regression, \
     chi2
+from sklearn.neural_network import MLPRegressor
 
 
 def feature_selection(data, target):
-    new_input = SelectKBest(f_classif, k=10)
+    new_input = SelectKBest(f_regression, k=10)
     new_input.fit(data, target)
     result = new_input.transform(data)
 
@@ -27,27 +31,18 @@ def normalize_data(input_data):
     # normalize the values (x-mean)/deviation
     normalized = [[(x - means[y.index(x)]) / sqrt[y.index(x)] for x in y] for y in input_data]
 
+    return normalized, means, sqrt
+
+
+def minmax_normalize(input_data):
+    maximums = np.max(input_data, axis=0)
+    normalized = [[x / maximums[y.index(x)] for x in y] for y in input_data]
+    return normalized, maximums
+
+
+def minmax_denormalize(input_data, maximums):
+    normalized = [[x * maximums[y.index(x)] for x in y] for y in input_data]
     return normalized
-
-
-def create_nn(input_data, output_data, path, hidden_layers=9, epochs=1000):
-    # init the neural network
-    mins = np.min(input_data, axis=0).tolist()
-    maxs = np.max(input_data, axis=0).tolist()
-    minmax = [[x, maxs[mins.index(x)]] for x in mins]
-
-    net = nl.net.newff(minmax, [hidden_layers, len(output_data[0])])
-    net.trainf = nl.net.train.train_cg
-    net.init()
-    # train the neural network
-    net.train(input_data, output_data, epochs=epochs, show=10, goal=0.1)
-    # save network to a file
-    print("\tSaving network in " + path)
-    net.save(path)
-    # get the results
-    res = net.sim(input_data)
-
-    return net, res
 
 
 def find_minmax(input):
@@ -57,14 +52,46 @@ def find_minmax(input):
     return minmax
 
 
-def create_neural_network(input_data, target_data, minmax, path, hidden_layers=10, epochs=1000):
+def create_neural_network(input_data, target_data, minmax, path, hidden_layers=7, epochs=800):
     # init the neural network
+    if os.path.isfile(path):
+        print("\n\tLoading network from " + path)
+        net = nl.load(path)
+        res = net.sim(input_data)
+        return net, res
 
     net = nl.net.newff(minmax, [hidden_layers, len(target_data[0])])
-    net.trainf = nl.net.train.train_cg
+    # net.transf = nl.net.trans.SoftMax
+    net.trainf = nl.net.train.train_rprop
+    # net.errorf = nl.net.error.CEE
+    # net.layers[-1].transf = nl.trans.SoftMax()
     net.init()
     # train the neural network
-    net.train(input_data, target_data, epochs=epochs, show=10, goal=0.2)
+    err = net.train(input_data, target_data, epochs=epochs, show=10, goal=1)
+    pl.figure(1)
+    pl.plot(err)
+    pl.show()
+    print("\tSaving network in " + path)
+    net.save(path)
+    # get the results
+    res = net.sim(input_data)
+
+    return net, res
+
+
+def denormalize_data(input_data, means, sqrt):
+    denormalized = [[x * sqrt[y.index(x)] + means[y.index(x)] for x in y] for y in input_data]
+    return denormalized
+
+
+def create_nn(input_data, output_data, path, hidden_layers=8, epochs=500, goal=0.5):
+    # init the neural network
+    net = nl.net.newff([[-100, 100]] * len(input_data[0]), [hidden_layers, len(output_data[0])])
+    net.trainf = nl.net.train.train_rprop
+    net.init()
+    # train the neural network
+    net.train(input_data, output_data, epochs=epochs, show=10, goal=goal)
+
     # save network to a file
     print("\tSaving network in " + path)
     net.save(path)
@@ -84,3 +111,14 @@ def divide_data_set(data_set):
     test_set = data_set[second_stop:-1]
 
     return train_set, validation_set, test_set
+
+
+def get_data(x_data, y_data):
+    reduces = [y[0] for y in y_data]
+    fs = feature_selection(np.array(x_data), np.array(reduces))
+    n_x_data, means_x, sqrt_x = normalize_data(fs.tolist())
+    n_y_data, means, sqrt = normalize_data(y_data)
+
+    x_train, x_validation, x_test = divide_data_set(n_x_data)
+    y_train, y_validation, y_test = divide_data_set(n_y_data)
+    return x_train, x_validation, x_test, y_train, y_validation, y_test, means, sqrt
